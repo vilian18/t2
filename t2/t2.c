8d4e01fc7bb5b93b9c247df632bcff4e4ddda147
vilian18 <vpavanatto@inf.ufsm.br>
2015-03-23 21:37:57 -0300

    funcoes de recebimento de arquivos prontas

--- t2/t2.c
+++ t2/t2.c
@@ -1,57 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAM_STRING 50
+#define TAM_FILE 30
 
-void testa_alocacao_mat(int* vetor){
-   if(vetor==NULL){
-      printf("Impossivel alocar a memoria\n Encerrando...\n");
-      exit(1);
-   }
-}
-
-void testa_alocacao_notas(float* vetor){
-   if(vetor==NULL){
-      printf("Impossivel alocar a memoria\n Encerrando...\n");
-      exit(1);
-   }
-}
-
-void testa_alocacao_nomes(char** vetor){
-   if(vetor==NULL){
-      printf("Impossivel alocar a memoria\n Encerrando...\n");
-      exit(1);
-   }
-}
-
-void aloca_notas(float* vetor, int n){
-   vetor= (float*)malloc(n * sizeof(float));
-   testa_alocacao_notas(vetor);
-}
-
-void aloca_vetor_mat(int* vetor, int n){
-   vetor= (int*)malloc(n* sizeof(int));
-   testa_alocacao_mat(vetor);
-}
-
-void aloca_vetor_nomes(char** vetor, int n, int tam_string){
+/*void aloca_vetor_nomes(char** vetor){
    int i=0;
-   vetor= (char**)malloc(n*sizeof(char*));
-   for(i=0; i<n; i++){
-      vetor[i]=(char*)malloc(tam_string*sizeof(char));
+   vetor= (char**)malloc(TAM_FILE*sizeof(char*));
+   for(i=0; i<; i++){
+      vetor[i]=(char*)malloc(TAM_STRING*sizeof(char));
    }
    testa_alocacao_nomes(vetor);
-}
+}*/
 
 void le_notas(int* matriculas, float* notas){
-   int mat, i=0;
-   float nota1= 0.0, nota2= 0.0;
+   int i=0, mat;
+   float nota1=0.0, nota2=0.0;
    FILE *f= fopen("notas.txt", "r");
    if(f==NULL){
-      printf("Erro ao abrir arquivo\n");
+      printf("Erro ao abrir arquivo!\n");
       exit(1);
    }
    while(feof(f)==0){
-      if(fscanf(f, "%d" "%f" "%f", &mat, &nota1, &nota2)<0)
+      if (fscanf(f,"%d %f %f\n", &mat, &nota1, &nota2)<0)
          break;
       matriculas[i]= mat;
       notas[i]= (nota1+nota2)/2;
@@ -60,19 +31,50 @@ void le_notas(int* matriculas, float* notas){
    fclose(f);
 }
 
+void le_alunos(int* matriculas, char** nomes, int* n){
+   int mat, i, linha=0;
+   char nome[TAM_STRING];
+   char c;
+   FILE *f;
+   f = fopen("alunos.txt", "r");
+   if(f==NULL){
+      printf("Erro ao abrir arquivo.\n");
+      exit(1);
+      }
+   while(feof(f)==0){
+      if(fscanf(f, "%d", &mat)<0)
+         break;
+      i=0;
+      c=fgetc(f);
+      while(c != '\n'){
+         nome[i]=c;
+         i++;
+         c=fgetc(f);
+      }
+      nome[i]='\0';
+      matriculas[linha]=mat;
+      strcpy(nomes[linha], nome);
+      linha++;
+   }
+   *n=linha;
+   fclose(f);
+}
 
 int main(int argc, char** argv){
-   int* vetor_matriculas;
-   int tam_string= 50;
-   int n=50;
+   int n=0, cont;
+   int* vetor_matric_notas;
+   int* vetor_matric_nomes;
    float* vetor_notas;
    char** vetor_nomes;
-   aloca_vetor_nomes(vetor_nomes, n, tam_string);
-   aloca_notas(vetor_notas, n);
-   aloca_vetor_mat(vetor_matriculas, n);
-   le_notas(vetor_matriculas, vetor_notas);
-   free(vetor_matriculas);
-   free(vetor_nomes);
-   free(vetor_notas);
+   vetor_matric_notas= (int*)malloc(TAM_FILE*sizeof(float));
+   vetor_matric_nomes= (int*)malloc(TAM_FILE*sizeof(int));
+   vetor_notas= (float*)malloc((TAM_FILE*sizeof(float)));
+   vetor_nomes= (char**)malloc(TAM_FILE*sizeof(char*));
+   for(cont=0; cont<TAM_FILE; cont++){
+      vetor_nomes[cont]= (char*)malloc(TAM_STRING*sizeof(char));
+   }
+   le_alunos(vetor_matric_nomes, vetor_nomes, &n);
+   le_notas(vetor_matric_notas, vetor_notas);
+
    return 0;
 }
